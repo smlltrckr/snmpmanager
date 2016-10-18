4c5585191efa824f5d70f7424b997ffbd9f931c3
Dustin <dustin@debian>
2016-10-18 00:12:27 -0700

    Neighbors and Devices started

--- snmpmanager.c
+++ snmpmanager.c
@@ -5,6 +5,31 @@
 #include <time.h>
 #include <math.h>
 
+#define MAX_IPV4 15
+
 int main(int argc, char ** argv){
 	return 0;
+}
+
+/*********************Find Devices*******************
+This function finds a device and returns it to a list
+	PRE:
+	POST:
+*/
+void findDevice(){
+	
+	return 0;
+}
+
+/********************Find Neighbor*******************
+This function finds the neighbor for each device
+	PRE: device identifier (String?)
+	POST: Array of Devices returned
+*/
+char **deviceNeighbors(char *device){
+	char **neighbors;
+	char *neighbors = (char *)malloc(MAX_IPV4 * sizeof(char**))
+	//char **neighbors = ( Need to allocate
+
+	return neighbors;
 }
